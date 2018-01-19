dbfb6b9cfb15c7f9bb296dad6a63eb1d75a54c21
Alex Lee <alee490@my.bcit.ca>
2018-01-19 10:07:51 -0800

    Modifications, logos to the page

--- login.php
+++ login.php
@@ -5,36 +5,39 @@
     $_SESSION['count']=0;
     $_SESSION['score']=0;
 ?>
-<!DOCTYPE html> 
-<html lang="en"> 
-<head> 
-    <title>Math Game</title> 
-    <meta charset="utf-8"> 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Math Game</title>
+    <meta charset="utf-8">
     <link rel="stylesheet" href="Styles/bootstrap.css">
     <link rel="stylesheet" href="Styles/mathgame.css">
-</head> 
-<body> 
-    <div class= "container">
+</head>
+
+<body>
+    <div class="container">
         <div class="row">
-            <div class="col-xs-4 offset-xs-4">  
-                <H1 class="text-center">Please login to enjoy our math game</H1>
-                <div class= "form-group">
+           <img class="center-block img-responsive" src="images/logoadjust.png">
+                <br />
+            <div class="col-sm-6 col-sm-offset-3">
+                
+                <div class="form-group">
                     <form action="index.php" method="post">
-                        <label for="username">Username:</label><input class="form-control" name="username">
-                        <label for="password">Password:</label><input class="form-control" type="password" name="password"><br/>
-                        <button type="submit" class="btn btn-info"name="login">Login</button><br/><br/>
-                        </form>
-                        <form action ="signup.php" method="post"><button type="submit" class="btn btn-info"name="signup">Sign Up</button>
-                </form>
+                        <label for="username">Username:</label><input class="form-control" name="username"><br />
+                        <label for="password">Password:</label><input class="form-control" type="password" name="password"><br /><br />
+                        <button type="submit" class="btn btn-info btn-group-justified" name="login">Login</button><br/>
+                    </form>
+                    <form action="signup.php" method="post"><button type="submit" class="btn btn-info btn-group-justified" name="signup">Sign Up</button>
+                    </form>
                     <br/>
-                                    <form method="Post" action="topscores.php">
-                                    <button class="btn btn-info" name="topscores" type="submit">Top Scores</button></form>
+                    <form method="Post" action="topscores.php">
+                        <button class="btn btn-success btn-group-justified" name="topscores" type="submit">Top Scores</button></form>
                 </div>
                 <?php echo '<span class="warning">'.$_GET['error'].'</span>';?>
-            </div>                     
+            </div>
         </div>
     </div>
 </body>
-</html>
 
-  
+</html>
